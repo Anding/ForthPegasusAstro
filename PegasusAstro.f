eb741e2e6278bee1ee6614ca3dfefabc02dc096b
Anding <andrew81244@outlook.com>
2024-10-26 20:45:39 +0800

    Tested and corrected command protocol

--- PegasusAstro.f
+++ PegasusAstro.f
@@ -1,9 +1,11 @@
 \ Use the serial port (COM) functionality of VFX Forth to communicate with a Pegasus Astro USB light box
+include %idir%\..\ForthBase\libraries\libraries.f
+
 NEED serial
 NEED CommandStrings
 
 \ COM port number, to revise on the local machine
-	7 value Pegasus.COM 
+	5 value Pegasus.COM 
 
 \ prepare an instance of a VFX Forth generic I/O driver
 	serdev: Pegasus.sid
@@ -14,7 +16,7 @@ NEED CommandStrings
 
 : add-lightbox ( --)
 \ open the serial port
-	Pegasus.com 9600 sid_Pegasus ( com_port baud) open-serial
+	Pegasus.com 9600 Pegasus.sid ( com_port baud) open-serial
 ;
 
 : remove-lightbox
@@ -27,40 +29,49 @@ NEED CommandStrings
 	Pegasus.sid ( addr n sid) write-gio ( ior) ABORT" Failed to write Pegasus COM port"
 ;
 
-: lightbox-ask ( -- n)
+: lightbox-ask ( addr -- addr n)		\ factor this to serial.f
 \ read from the lightbox.  non-blocking with brief hardware timeout
+	dup
 	10 BEGIN								\ timeout counter
-		Pegasus.sed key?-gio
+		Pegasus.sid key?-gio
 	0= WHILE
 		1- dup 0= ( ior) ABORT" Pegasus COM port has not responded after 1000 ms"
 		100 ms
 	REPEAT
 	drop
-	Pegasus.buffer 256 Pegasus.sid ( addr n sid) readex-gio ( n ior) ABORT" Failed read from Pegasus COM port"
+	
+	BEGIN									\ read while characters are available
+		Pegasus.sid key?-gio
+	WHILE
+		Pegasus.sid ( addr addr' sid) key-gio ( addr addr c)
+		over c!
+		1+
+		1 ms								\ appropriate for 9600 baud
+	REPEAT
+		over - ( addr n)
 ;
 
 : lightbox-on ( --)
- 	<< 'E' | ':' | '1' | 0x0d | >>
+ 	Pegasus.command << 'E' | ':' | '1' | 0x0a | >>
 	lightbox-tell
 
 ;
 : lightbox-off ( --)
- 	<< 'E' | ':' | '0' | 0x0d | >>
+ 	Pegasus.command << 'E' | ':' | '0' | 0x0a | >>
 	lightbox-tell
 ;
 
-: ->lighbox.intensity ( n --)	
+: ->lightbox.intensity ( n --)	
 \ n is the intensity percentage 0 <= n <= 100
-	100 - ( dark%) 235 * 100 / 20 + ( darkness in range 20-255)
-	<< 'L' | ':' | (.) ..| 0x0d | >>
+	100 swap - ( dark%) 235 * 100 / 20 + ( darkness in range 20-255)
+	Pegasus.command << 'L' | ':' | (.) ..| 0x0a | >>
 	lightbox-tell
 ;
 
 : lightbox.version ( -- caddr u)
 \ report the firmware version number
-	<< 'V' | 0x0d | >>
+	Pegasus.command << 'V' | 0x0a | >>
 	lightbox-tell
 	200 ms
-	lightbox-ask
-	lightbox-buffer 2 + swap 2 -
+	Pegasus.buffer lightbox-ask
 ;
